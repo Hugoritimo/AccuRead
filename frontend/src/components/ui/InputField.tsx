d65c3d3d7ebfb2d51a6770ee58e6466187084731
Victor Hugo <80632092+Hugoritimo@users.noreply.github.com>
2024-09-17 15:03:00 -0300

    MUDANÇAS

--- frontend/src/components/ui/InputField.tsx
+++ frontend/src/components/ui/InputField.tsx
@@ -1,15 +1,16 @@
 import React from "react";
-import { Input } from "@/components/ui/Input";
-import { Label } from "@/components/ui/Label";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 interface InputFieldProps {
   label: string;
   name: string;
   value: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  placeholder?: string;
   type?: string;
-  required?: boolean;
+  readOnly?: boolean;
+  required?: boolean; // Adicionando required como opcional
+  icon?: JSX.Element; // Tornando icon opcional
 }
 
 const InputField: React.FC<InputFieldProps> = ({
@@ -17,25 +18,28 @@ const InputField: React.FC<InputFieldProps> = ({
   name,
   value,
   onChange,
-  placeholder = "",
   type = "text",
-  required = false,
-}) => {
-  return (
-    <div className="flex flex-col">
-      <Label htmlFor={name}>{label}</Label>
-      <Input
-        id={name}
-        name={name}
-        value={value}
-        onChange={onChange}
-        placeholder={placeholder}
-        type={type}
-        className="mt-2"
-        required={required}
-      />
-    </div>
-  );
-};
+  readOnly = false,
+  required = false, // Valor padrão definido para false
+  icon,
+}) => (
+  <div className="mb-4">
+    <Label className="block text-sm font-medium mb-2 text-gray-700 flex items-center">
+      {icon && <span className="mr-2">{icon}</span>}{" "}
+      {/* Renderizar o ícone se ele for passado */}
+      <span>{label}</span>
+    </Label>
+    <Input
+      type={type}
+      name={name}
+      placeholder={label}
+      className="w-full border-gray-300 focus:border-indigo-600 focus:ring focus:ring-indigo-600 focus:ring-opacity-50"
+      value={value}
+      onChange={onChange}
+      readOnly={readOnly}
+      required={required} // Passando a propriedade required ao Input
+    />
+  </div>
+);
 
 export default InputField;
